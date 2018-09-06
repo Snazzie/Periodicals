7dc7023c444ddbba7817e2ad40e61525dd971e0a
Aaron Cooper <aaron.cooper@red-gate.com>
2018-09-06 10:25:54 +0100

    Added unit test project

---
+++ PeriodicalsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeriodicalsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Redgate")]
+[assembly: AssemblyProduct("PeriodicalsTest")]
+[assembly: AssemblyCopyright("Copyright © Redgate 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08542e28-637a-4008-9ab2-ed50f1efc3f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
