970f32aae4fb7bf6b48db00ed3657cf2c1fe9cd0
Aaron Cooper <aaron.cooper@red-gate.com>
2018-09-06 13:52:18 +0100

    added arrange act assert comment

--- PeriodicalsTest/ProgramTest.cs
+++ PeriodicalsTest/ProgramTest.cs
@@ -19,6 +19,7 @@ namespace PeriodicalsTest
         [Test]
         public void GetMagazineMonthlyRevenueInYear_ReturnsCorrectMonthlyRevenues()
         {
+            // ARRANGE
             var year = 2017;
             var user = new User("Jeff");
             var user2 = new User("Geo");
@@ -31,9 +32,11 @@ namespace PeriodicalsTest
                 new Subscription(user, magazine2, Convert.ToDateTime("01/09/2014"), Convert.ToDateTime("01/09/2017"))
             };
 
-
+            // ACT
             List<float> rev = Program.GetMagazineMonthlyRevenueInYear(magazine, year, subscriptions);
             List<float> rev2 = Program.GetMagazineMonthlyRevenueInYear(magazine2, year, subscriptions);
+
+            // ASSERT
             Assert.AreEqual((magazine.Price / 12) * 12, rev.Sum());
             Assert.AreEqual((magazine2.Price / 12) * (9 + 12), rev2.Sum());
         }
