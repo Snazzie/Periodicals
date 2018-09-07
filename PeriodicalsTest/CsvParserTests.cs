39b2434cf5e3483294f25b174893cb555918d287
Aaron Cooper <aaron.cooper@red-gate.com>
2018-09-07 17:07:00 +0100

    Test for converting line to subscription object is correct

--- PeriodicalsTest/CsvParserTests.cs
+++ PeriodicalsTest/CsvParserTests.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Periodicals;
 using Periodicals.Magazines;
+using Periodicals.Subscriptions;
+using Periodicals.Users;
 
 namespace PeriodicalsTest
 {
@@ -33,9 +34,27 @@ namespace PeriodicalsTest
         [TestCase("Joan Krook,HelloWorld,20/01/2017,20/09/2017", ExpectedResult = true)]
         [TestCase("Joan Krook,\"HelloWorld\",20/01/2017,20/09/2017", ExpectedResult = true)]
         [TestCase("Joan Krook,\"Hello, World\",20/01/2017,20/09/2017", ExpectedResult = true)]
-        public bool TryCreateSubscriptionFromLine_ReturnsSubscription(string lines)
+        public bool TryCreateSubscriptionFromLine_ReturnsSubscription(string line)
         {
-            return CsvParser.TryCreateSubscriptionFromLine(MagazineService, lines, out _);
+            return CsvParser.TryCreateSubscriptionFromLine(MagazineService, line, out _);
+        }
+        [Test]
+        public void TryCreateSubscriptionFromLine_ReturnedSubscriptionIsCorrect()
+        {
+            // ARRANGE
+            var line = "Joan Krook,Amiga Gamer,20/01/2017,20/09/2017";
+            var user = new User("Joan Krook");
+            var expected = new Subscription(MagazineService, user, "Amiga Gamer", DateTime.Parse("20/01/2017"),
+                DateTime.Parse("20/09/2017"));
+
+            // ACT
+            CsvParser.TryCreateSubscriptionFromLine(MagazineService, line, out var subscription);
+
+            // ASSERT
+            Assert.That(subscription.User.Name, Is.EqualTo(expected.User.Name));
+            Assert.That(subscription.Magazine.Title, Is.EqualTo(expected.Magazine.Title));
+            Assert.That(subscription.StartDate, Is.EqualTo(expected.StartDate));
+            Assert.That(subscription.EndDate, Is.EqualTo(expected.EndDate));
         }
     }
 }
